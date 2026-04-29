fcee64b16049dc7ceb3380acb1b5e6566493d0b0
ahmad elsayed <ahmdalsayed92@gmail.com>
2026-04-29 12:22:19 +0300

    Add root route to return backend status

--- server.js
+++ server.js
@@ -290,3 +290,7 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🎮 الهبيدة server running on port ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
